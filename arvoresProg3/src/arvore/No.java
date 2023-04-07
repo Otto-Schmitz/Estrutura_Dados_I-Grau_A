158bb107d566f42842ff970cf8fd7f5587813e5b
Otto Schmitz <otto.schmitz@cwi.com.br>
2023-04-07 16:59:26 -0300

    

---
+++ arvoresProg3/src/arvore/No.java
@@ -0,0 +1,19 @@
+package arvore;
+
+public class No {
+	
+	int numero;
+	int altura;
+	No noEsquerdo;
+	No noDireito;
+	
+	public No() {
+		
+	}
+	
+	public No(int numero) {
+		this.numero = numero;
+	}
+	
+	
+}
---
+++ arvoresProg3/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import arvore.ConstrutorArvore;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ConstrutorArvore arvore = new ConstrutorArvore();
+		
+		arvore.inserirNo(30, arvore.getRaiz());
+		arvore.inserirNo(25, arvore.getRaiz());
+	}
+
+}
